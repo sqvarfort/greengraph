aca9cd4b5caae57ea20eccb063e6067ed6385880
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-01-01 21:53:49 +0100

    Added source code

---
+++ greengraph/graph.py
@@ -0,0 +1,39 @@
+import numpy as np
+import geopy
+from map import Map
+
+# This is the constructor
+# Should we divide these two into two files? I would
+# Create one map object and one greengraph object
+
+
+# Create the Greengraph object
+class Greengraph(object):
+    def __init__(self, start, end): #These is the main object that I create with the start and end location
+        self.start=start # Declare local variables?
+        self.end=end
+        self.geocoder=geopy.geocoders.GoogleV3(domain="maps.google.co.uk")  #Create object, geocoder that belongs to the class, and to which geocoders sets attributes that tell you about the locaion of a place
+# What to test: that it is given the correct domain name...
+# However, domain name is not user input...
+
+
+    def geolocate(self, place): #Find the place, will be used later on, just for syntax simplicity
+        return self.geocoder.geocode(place,exactly_one=False)[0][1]
+# This we can definitely test
+
+
+    def location_sequence(self, start,end,steps): #Denote the latitude/longitude
+        lats = np.linspace(start[0], end[0], steps) # np.linspace returns evenly spaced numbers over an interval
+        longs = np.linspace(start[1],end[1], steps)
+        return np.vstack([lats, longs]).transpose()
+# Returns a matrix of the longitudes and latitudes in a matrix, where they are the columns, not rows
+# vstack has the function of stacking the arrays in the rows of a matrix.
+
+
+# This returns an array with two values: the steps and the
+    def green_between(self, steps):
+        return [Map(*location).count_green()
+        for location in self.location_sequence(
+        self.geolocate(self.start),
+        self.geolocate(self.end),steps)]
+        #
---
+++ greengraph/map.py
@@ -0,0 +1,43 @@
+import numpy as np
+import requests
+from StringIO import StringIO
+from matplotlib import image as img
+
+
+class Map(object): #Obtain the map from Google Maps
+    def __init__(self, lat, long, satellite=True,
+        zoom=10, size=(400,400), sensor=False): #Tells us the dimensions of the map required
+        #These values are the default, unless specified anew by the user
+        base="http://maps.googleapis.com/maps/api/staticmap?" #domin address
+        params=dict( #Create a dictionary with all the values, ot be passed to the server
+                sensor= str(sensor).lower(),
+                zoom= zoom,
+                size= "x".join(map(str, size)),
+                center= ",".join(map(str, (lat, long) )),
+                style="feature:all|element:labels|visibility:off"
+        )
+
+        if satellite: #Make sure that the map returned is a satellite image
+            params["maptype"]="satellite"
+        self.image = requests.get(base, params=params).content
+        # Fetch our PNG image data
+        self.pixels= img.imread(StringIO(self.image))
+        # Parse our PNG image as a numpy array
+
+    def green(self, threshold):
+        # Use NumPy to build an element-by-element logical array
+        #Presumably, we are using the three colours that go into each pixel,
+        greener_than_red = self.pixels[:,:,1] > threshold* self.pixels[:,:,0] # set to 1 if green
+        greener_than_blue = self.pixels[:,:,1] > threshold*self.pixels[:,:,2] # set to 1 if green
+        green = np.logical_and(greener_than_red, greener_than_blue) #Only if they are both true, we set to one
+        return green
+
+    def count_green(self, threshold = 1.1): #Count the number of green pixels
+        return np.sum(self.green(threshold)) #Sum up all the ones in the matrix
+
+    def show_green(data, threshold = 1.1): #Not sure if this function is even useful....
+        green = self.green(threshold)
+        out = green[:,:,np.newaxis]*array([0,1,0])[np.newaxis,np.newaxis,:]
+        buffer = StringIO()
+        result = img.imsave(buffer, out, format='png')
+        return buffer.getvalue()
