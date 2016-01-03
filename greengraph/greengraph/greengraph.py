2d072197b9884f24be6a2b0328f49232af5e7c21
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-01-03 21:50:39 +0100

    Program is working with parser

--- greengraph/greengraph/greengraph.py
+++ greengraph/greengraph/greengraph.py
@@ -1,14 +1,12 @@
-#Here we set up the parser
+## Greengraph
+#----------------------------
+# Description:
 
 from argparse import ArgumentParser
 import map
 import numpy as np
 from graph import Greengraph
 import matplotlib.pyplot as plt
-import scipy
-
-# parser
-# Need to also add the command to decide the image
 
 
 if __name__ == "__main__":
@@ -16,16 +14,14 @@ if __name__ == "__main__":
     parser.add_argument('start', help = 'Input start location')
     parser.add_argument('end', help = 'Input end location')
     parser.add_argument('filename', type=str, help ='image filename with extension')
-    parser.add_argument('--steps', type=int, help = 'Number of steps, default = 20')
+    parser.add_argument('--steps', '-s', type=int, help = 'Number of steps, default = 20')
     parser.add_argument('--plot', '-p', action ='store_true', help ='Display plot')
-    #parser.add_argument('--image_type', type=str, help = 'File format of output image')
+
     arguments = parser.parse_args()
 
-    #img_file = arguments.filename +'.' + arguments.image_type
+    mygraph = Greengraph(arguments.start, arguments.end) # Create Greengraph object
 
-
-    mygraph = Greengraph(arguments.start, arguments.end)
-
+    # If word not recognised,
     # Set stepsize if entered. Otherwise, set to 20.
     if arguments.steps:
         data = mygraph.green_between(arguments.steps)
