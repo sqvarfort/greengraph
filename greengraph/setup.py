e765c909d43872cfbb7e6cfd40abe2ea392c7a3f
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-01-05 09:01:04 +0100

    Added another package to setup.py

--- greengraph/setup.py
+++ greengraph/setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name = 'Greengraph',
     version = '0.1',
-    packages = find_packages(excplude=['*test']), #Ignore the test-file
+    description = 'Greengraph - counts green space between two locations'
+    packages = find_packages(exclude=['*test']), #Ignore the test-file
     # scripts = ['scripts/Greengraph'], # Note sure yet if needed
-    install_requires = ['argparse']
+    install_requires = ['argparse', 'geopy']
 )
