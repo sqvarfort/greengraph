a041f882c584cccdb106930c2dbaa25dc9b88e8a
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-01-07 12:38:15 +0000

    Added way to send user API overload warning

--- greengraph/greengraph/greengraph.py
+++ greengraph/greengraph/greengraph.py
@@ -8,7 +8,7 @@ import numpy as np
 from graph import Greengraph
 import matplotlib.pyplot as plt
 
-
+# Create parser arguments
 if __name__ == "__main__":
     parser = ArgumentParser(description = "Count the green space between two locations")
     parser.add_argument('start', help = 'Input start location')
@@ -19,16 +19,29 @@ if __name__ == "__main__":
 
     arguments = parser.parse_args()
 
+
     mygraph = Greengraph(arguments.start, arguments.end) # Create Greengraph object
 
-    # If word not recognised,
+
+
     # Set stepsize if entered. Otherwise, set to 20.
     if arguments.steps:
         data = mygraph.green_between(arguments.steps)
     else:
         data = mygraph.green_between(20)
 
-    ## Saving and plotting data
+    # Catch API overload error
+    # Google sends error picture with 325 green pixels.
+    error_threshold = 5
+    count = 0
+    for iterate in range(0,arguments.steps):
+        if data[iterate] == 323:
+            count += 1
+    if count >= error_threshold:
+        print 'Warning: API overload'
+
+
+    # Saving and plotting data
     fig = plt.figure()
     plt.plot(data)
     fig.suptitle('Greengraph - %s to %s' %(arguments.start, arguments.end), fontsize=14, fontweight='bold')
@@ -37,5 +50,5 @@ if __name__ == "__main__":
     ax.set_ylabel('green pixel count')
     plt.savefig(arguments.filename) # Save plot
 
-    if arguments.plot: # Show plot
+    if arguments.plot: # Show plot if arguments.plot = TRUE
         plt.show()
