ac3d4a708d6a9b09f9fe2aaaaa8573fd7471b673
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-01-10 23:13:09 +0000

    Cleaned up comments and added some explanations.

--- greengraph/greengraph/map.py
+++ greengraph/greengraph/map.py
@@ -7,10 +7,9 @@ import time
 
 class Map(object): #Obtain the map from Google Maps
     def __init__(self, lat, long, satellite=True,
-        zoom=10, size=(400,400), sensor=False): #Tells us the dimensions of the map required
-        #These values are the default, unless specified anew by the user
+        zoom=10, size=(400,400), sensor=False):
         base="http://maps.googleapis.com/maps/api/staticmap?" #domin address
-        params=dict( #Create a dictionary with all the values, ot be passed to the server
+        params=dict( 
                 sensor= str(sensor).lower(),
                 zoom= zoom,
                 size= "x".join(map(str, size)),
@@ -27,17 +26,16 @@ class Map(object): #Obtain the map from Google Maps
 
     def green(self, threshold):
         # Use NumPy to build an element-by-element logical array
-        #Presumably, we are using the three colours that go into each pixel,
         greener_than_red = self.pixels[:,:,1] > threshold* self.pixels[:,:,0] # set to 1 if green
         greener_than_blue = self.pixels[:,:,1] > threshold*self.pixels[:,:,2] # set to 1 if green
-        green = np.logical_and(greener_than_red, greener_than_blue) #Only if they are both true, we set to one
+        green = np.logical_and(greener_than_red, greener_than_blue)
         return green
 
     def count_green(self, threshold = 1.1): #Count the number of green pixels
-        return np.sum(self.green(threshold)) #Sum up all the ones in the matrix
+        return np.sum(self.green(threshold))
 
 
-    def show_green(self, threshold = 1.1): #Not sure if this function is even useful....
+    def show_green(self, threshold = 1.1): #Show a green pixel map
         green = self.green(threshold)
         out = green[:,:,np.newaxis]*np.array([0,1,0])[np.newaxis,np.newaxis,:]
         buffer = StringIO()
