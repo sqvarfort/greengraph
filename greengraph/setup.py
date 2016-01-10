fddcac693d92e3acf780e9ef7121045396680fcb
Sofia Qvarfort <sofia.qvarfort.15@ucl.ac.uk>
2016-01-10 23:15:19 +0000

    Final commit

--- greengraph/setup.py
+++ greengraph/setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'Greengraph',
     version = '0.1',
-    description = 'Greengraph - counts green space between two locations'
-    packages = find_packages(exclude=['*test']), #Ignore the test-file
-    # scripts = ['scripts/Greengraph'], # Note sure yet if needed
+    description = 'Greengraph - counts green space between two locations',
+    packages = find_packages(exclude=['*test']),
     install_requires = ['argparse', 'geopy']
 )
